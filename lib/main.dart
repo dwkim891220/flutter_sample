8ef022f53d3cc719530cfb23793edad84d07f5a6
dowoo kim <dowoo.kim.891220@gmail.com>
2022-12-14 14:33:24 +0900

    Feat foreground notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,157 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_sample/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(const MyApp());
+  await _initFcm();
+
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  _parseInitialMessage(message);
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+_initFcm() async {
+  final authorized = await _requestPermissions();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final token = await FirebaseMessaging.instance.getToken();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  if (authorized) {
+    AndroidNotificationPlugin? androidNotification;
 
-  final String title;
+    if (Platform.isAndroid) {
+      androidNotification = await _initAndroidNotification();
+    }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+    if (Platform.isIOS) {
+      await _initIOSNotification();
+    }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    FirebaseMessaging.instance.subscribeToTopic('global');
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+    if (initialMessage != null) {
+      _parseInitialMessage(initialMessage);
+    }
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      _parseInitialMessage(message);
+    });
+
+    FirebaseMessaging.onMessage.listen((message) {
+      _showLocalNotification(message, androidNotification);
     });
   }
+}
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+Future<bool> _requestPermissions() async {
+  final fcmInstance = FirebaseMessaging.instance;
+
+  if (Platform.isIOS) {
+    await fcmInstance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    await fcmInstance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
     );
   }
+
+  final settings = await fcmInstance.getNotificationSettings();
+  return settings.authorizationStatus == AuthorizationStatus.authorized;
+}
+
+_parseInitialMessage(RemoteMessage message) {
+  String msg = message.notification?.body ?? 'test';
+  //TODO
+}
+
+Future<AndroidNotificationPlugin> _initAndroidNotification() async {
+  final plugin = FlutterLocalNotificationsPlugin();
+  final notification = AndroidNotificationPlugin(
+    plugin: plugin,
+  );
+
+  await notification.plugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(notification.channel);
+
+  await plugin.initialize(
+    const InitializationSettings(
+      android: AndroidInitializationSettings('ic_notification'),
+    ),
+    onDidReceiveNotificationResponse: (details) => _clickLocalNotification,
+  );
+
+  return notification;
+}
+
+_initIOSNotification() async {
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+}
+
+_showLocalNotification(
+  RemoteMessage message,
+  AndroidNotificationPlugin? androidNotification,
+) {
+  RemoteNotification? notification = message.notification;
+
+  if (notification != null && androidNotification != null) {
+    final channel = androidNotification.channel;
+    final plugin = androidNotification.plugin;
+
+    plugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+        ),
+      ),
+      payload: jsonEncode(message.data),
+    );
+  }
+}
+
+_clickLocalNotification(NotificationResponse response) {
+  final payload = response.payload;
+  print("payload = $payload");
+}
+
+class AndroidNotificationPlugin {
+  const AndroidNotificationPlugin({
+    required this.plugin,
+  });
+
+  final FlutterLocalNotificationsPlugin plugin;
+  final AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    'channel_id',
+    'channel_title',
+    importance: Importance.max,
+  );
 }
